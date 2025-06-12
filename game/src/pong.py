3f35ed68d6104550cfb15f41c0922dedaaa80b43
Roman Mikhaylenko <roman.mikhaylenko14@hotmail.com>
2025-06-12 15:49:47 +0100

    add: speedup ball with more collisions

--- game/src/pong.py
+++ game/src/pong.py
@@ -9,7 +9,6 @@ def make_shape(topleft, width, height):
     return surf, surf.get_frect(topleft=topleft)
 
 
-
 class Ball(pygame.sprite.Sprite):
     def __init__(self, groups, pads, scores):
         super().__init__(groups)
@@ -18,11 +17,13 @@ class Ball(pygame.sprite.Sprite):
         self.image, self.rect = make_shape((start_x, start_y), BALL_SIZE, BALL_SIZE)
         self.direction = pygame.math.Vector2(1, -1)
         self.pad_left, self.pad_right = pads
-        self.game_active = False
         self.game_active_ever = False
+        self.game_active = False
         self.scores = scores
+        self.collisions = 0
 
     def pad_hit(self, pad_rect, ball_rect):
+        self.collisions += 1
         return pad_rect.top <= ball_rect.centery <= pad_rect.bottom
     
     def reset_position(self):
@@ -31,6 +32,7 @@ class Ball(pygame.sprite.Sprite):
         self.pad_left.reset_position()
         self.pad_right.reset_position()
         self.game_active = False
+        self.collisions = 0
 
     def update(self, delta_time):
         keys = pygame.key.get_pressed()
@@ -56,17 +58,17 @@ class Ball(pygame.sprite.Sprite):
                     self.scores["player1"] += 1
                     self.reset_position()
 
-            self.rect.center += self.direction * BALL_SPEED * delta_time
+            self.rect.center += self.direction * BALL_SPEED * delta_time * (1 + self.collisions / 10)
 
 
 class Pad(pygame.sprite.Sprite):
     def __init__(self, x, y, player, groups):
         super().__init__(groups)
-        self.player = player
-        self.image, self.rect = make_shape((x, y), PAD_WIDTH, PAD_HEIGHT)
+        self.start_x   = x
+        self.start_y   = y
+        self.player    = player
         self.direction = pygame.math.Vector2(0, 1)
-        self.start_x = x
-        self.start_y = y
+        self.image, self.rect = make_shape((x, y), PAD_WIDTH, PAD_HEIGHT)
 
     def reset_position(self):
         self.rect.topleft = (self.start_x, self.start_y)
@@ -86,19 +88,18 @@ class Text(pygame.sprite.Sprite):
     def __init__(self, pos, color, scores, ball, groups):
         super().__init__(groups)
         self.x, self.y  = pos
-        self.font       = pygame.font.Font(FONT_PATH, TEXT_SIZE)
         self.color      = color
         self.scores     = scores
         self.ball       = ball
-        self.image      = self.font.render("PRESS SPACE", True, self.color)
-        self.rect       = self.image.get_rect(center=(self.x, self.y))
+        self.font  = pygame.font.Font(FONT_PATH, TEXT_SIZE)
+        self.image = self.font.render("PRESS SPACE", True, self.color)
+        self.rect  = self.image.get_rect(center=(self.x, self.y))
 
     def update(self, *_):
         if self.ball.game_active_ever:
             text = f"{self.scores['player1']}:{self.scores['player2']}"
             self.image = self.font.render(text, True, self.color)
             self.rect  = self.image.get_rect(center=(WINDOW_WIDTH // 2, self.y))
-
 
 
 def main():
@@ -113,9 +114,10 @@ def main():
     # make pads & ball & text
     all_sprites = pygame.sprite.Group()
     pad_start_y = WINDOW_HEIGHT//2 - PAD_HEIGHT//2
-
-    pad_left  = Pad(0, pad_start_y, "player1", all_sprites)
-    pad_right = Pad(WINDOW_WIDTH - PAD_WIDTH, pad_start_y, "player2", all_sprites)
+    padl_start_x, padr_start_x = 0, WINDOW_WIDTH - PAD_WIDTH
+    
+    pad_left  = Pad(padl_start_x, pad_start_y, "player1", all_sprites)
+    pad_right = Pad(padr_start_x, pad_start_y, "player2", all_sprites)
     ball      = Ball(all_sprites, (pad_left, pad_right), scores)
     Text((WINDOW_WIDTH // 2, WINDOW_HEIGHT//10), TEXT_COLOR, scores, ball, all_sprites)
 
